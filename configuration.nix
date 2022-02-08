7e396d1b79f13d00789174c3322dfdb6b12d003c
Samuel Kyletoft <samuel@kyletoft.se>
2022-02-08 23:34:48 +0100

    Nix GC syntax fix

--- configuration.nix
+++ configuration.nix
@@ -17,14 +17,17 @@ in {
 	};
 
 	nix = {
-		autoOptimiseStore = true;
+		settings.auto-optimise-store = true;
 		gc = {
 			automatic = true;
 			dates = "weekly";
 			options = "--delete-older-than 30d";
 		};
 		# Free up to 1GiB whenever there is less than 100MiB left.
-		extraOptions = '' min-free = ${toString (100 * 1024 * 1024)} max-free = ${toString (1024 * 1024 * 1024)} '';
+		extraOptions = ''
+			min-free = ${toString (100 * 1024 * 1024)}
+			max-free = ${toString (1024 * 1024 * 1024)}
+		'';
 	};
 
 	# Use the systemd-boot EFI boot loader.
