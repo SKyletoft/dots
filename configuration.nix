d0ebfba0f551630ad727e69f35f6269537a25a5d
Samuel Kyletoft <samuel@kyletoft.se>
2022-02-08 23:23:49 +0100

    Nix: automatic GC

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,17 @@ in {
 		allowBroken = false;
 	};
 
+	nix = {
+		autoOptimiseStore = true;
+		gc = {
+			automatic = true;
+			dates = "weekly";
+			options = "--delete-older-than 30d";
+		};
+		# Free up to 1GiB whenever there is less than 100MiB left.
+		extraOptions = '' min-free = ${toString (100 * 1024 * 1024)} max-free = ${toString (1024 * 1024 * 1024)} '';
+	};
+
 	# Use the systemd-boot EFI boot loader.
 	boot = {
 		loader = {
