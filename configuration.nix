feec0b012f3ffce4a38260f5958a7df13a080b06
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-20 01:29:22 +0100

    Overlay for Sway on NVIDIA

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,13 @@ in {
 	nixpkgs.config = {
 		allowUnfree = true;
 		allowBroken = false;
+		overlays = [
+			(final: prev: {
+				wlroots = prev.wlroots.overrideAttrs(old: {
+					postPatch = "sed -i 's/assert(argb8888 &&/assert(true || argb8888 ||/g' 'render/wlr_renderer.c'";
+				});
+			})
+		];
 	};
 
 	nix = {
