845a8f545dcc657d4b2fc86b0262d876021777bc
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-03 21:38:03 +0200

    Add Eurydice

---
+++ eurydice.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, lib, ... }:
+
+{
+	imports = [
+		<nixos-hardware/raspberry-pi/4>
+		<nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+	];
+
+	nixpkgs.config.allowUnfree = true;
+	nix = {
+		settings.auto-optimise-store = true;
+		gc = {
+			automatic = true;
+			dates = "weekly";
+			options = "--delete-older-than 30d";
+		};
+		extraOptions = ''
+			min-free = ${toString (100 * 1024 * 1024)}
+			max-free = ${toString (1024 * 1024 * 1024)}
+		'';
+	};
+
+	networking = {
+		hostName = "eurydice";
+		interfaces.eth0.ipv4.addresses = [ {
+			address = "192.168.0.200";
+			prefixLength = 24;
+		} ];
+		defaultGateway = "192.168.0.1";
+		nameservers = [ "8.8.8.8" ];
+	};
+
+	fileSystems."/" = {
+		device = "/dev/disk/by-label/NIXOS_SD";
+		fsType = "ext4";
+		options = [ "noatime" ];
+	};
+
+	environment.systemPackages = with pkgs; [ neovim ];
+
+	users.users.u3836 = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+	};
+
+	hardware = {
+		pulseaudio.enable = false;
+		# Enable GPU acceleration
+		raspberry-pi."4".fkms-3d.enable = false;
+	};
+	powerManagement.cpuFreqGovernor = "ondemand";
+
+	services = {
+		xserver.enable = false;
+		openssh = {
+			enable = true;
+			passwordAuthentication = false;
+		};
+		earlyoom.enable = true;
+		ananicy = {
+			enable = true;
+			package = pkgs.ananicy-cpp;
+		};
+	};
+
+	security = {
+		sudo.enable = false;
+		doas = {
+			enable = true;
+			extraRules = [{
+				users = [ "u3836" ];
+				keepEnv = true;
+				persist = true;
+			}];
+		};
+	};
+}
