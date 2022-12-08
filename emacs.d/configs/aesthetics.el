f15f1c125c3f577ad0c056be3adde2260be6987d
Samuel Kyletoft <samuel@kyletoft.se>
2022-12-08 15:56:15 +0100

    Formatting

--- emacs.d/configs/aesthetics.el
+++ emacs.d/configs/aesthetics.el
@@ -2,10 +2,10 @@
 (add-to-list 'default-frame-alist '(font . "Cascadia Mono PL Semibold-11"))
 
 (set-frame-parameter (selected-frame)
-		     'alpha
-		     '(90 . 95))
+                     'alpha
+                     '(90 . 95))
 (add-to-list 'default-frame-alist
-	     '(alpha . (90 . 95)))
+             '(alpha . (90 . 95)))
 
 (load-theme 'custom-monokai t)
 (global-display-line-numbers-mode t)
