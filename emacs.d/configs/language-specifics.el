6b4603072af8138dcd63775e63650ba83f31e645
Samuel Kyletoft <samuel@kyletoft.se>
2022-12-18 03:19:08 +0100

    Rust lsp config

--- emacs.d/configs/language-specifics.el
+++ emacs.d/configs/language-specifics.el
@@ -44,48 +44,52 @@
             ;; (haskell-post-lsp)
             ))
 
-(add-hook 'rust-mode-hook
-          (lambda ()
-            (setq-local rust-indent-offset 8)
-            (setq-local tab-width 8)
-            (setq-local evil-shift-width 8)
-            (setq-local indent-tabs-mode t)))
-
 (use-package rustic
-  :ensure
-  :bind (:map rustic-mode-map
-              ("M-j" . lsp-ui-imenu)
-              ("M-?" . lsp-find-references)
-              ("C-c C-c l" . flycheck-list-errors)
-              ("C-c C-c a" . lsp-execute-code-action)
-              ("C-c C-c r" . lsp-rename)
-              ("C-c C-c q" . lsp-workspace-restart)
-              ("C-c C-c Q" . lsp-workspace-shutdown)
-              ("C-c C-c s" . lsp-rust-analyzer-status))
   :config
   ;; (setq lsp-eldoc-hook nil)
   ;; (setq lsp-enable-symbol-highlighting nil)
   ;; (setq lsp-signature-auto-activate nil)
   (setq rustic-format-on-save t)
   (setq lsp-rust-analyzer-server-display-inlay-hints t)
-  (add-hook 'rustic-mode-hook (lambda ()
-                                (when buffer-file-name
-                                  (setq-local buffer-save-without-query t))
-                                (add-hook 'before-save-hook 'lsp-format-buffer nil t))))
+  (add-hook 'rustic-mode-hook
+            (lambda ()
+              (when buffer-file-name
+                (setq-local buffer-save-without-query t))
+              (setq-local rust-indent-offset 8)
+              (setq-local tab-width 8)
+              (setq-local evil-shift-width 8)
+              (setq-local indent-tabs-mode t)
+
+              (setq-local lsp-rust-analyzer-cargo-watch-command "clippy")
+              (setq-local lsp-idle-delay 0.6)
+              (setq-local lsp-rust-analyzer-server-display-inlay-hints t)
+              (setq-local lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+              (setq-local lsp-rust-analyzer-display-chaining-hints t)
+              (setq-local lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names t)
+              (setq-local lsp-rust-analyzer-display-closure-return-type-hints t)
+              (setq-local lsp-rust-analyzer-display-parameter-hints t)
+              (setq-local lsp-rust-analyzer-display-reborrow-hints t)
+              (setq-local rustic-format-on-save nil)
+
+              (setq-local eldoc-mode nil)
+              (setq-local lsp-eldoc-enable-hover nil)
+              (setq-local eldoc-documentation-function #'ignore)
+              (setq-local lsp-ui-sideline-show-hover nil)
+              (setq-local lsp-ui-sideline-enable nil)
+
+              (define-key evil-normal-state-map (kbd "SPC i") 'rustic-format-buffer)
+              (define-key evil-normal-state-map (kbd "SPC f") 'lsp-ui-doc-glance)
+              (define-key evil-normal-state-map (kbd "SPC g") 'xref-find-definitions)
+              (define-key evil-normal-state-map (kbd "SPC a") 'lsp-execute-code-action)
+
+              (add-hook 'before-save-hook 'lsp-format-buffer nil t))))
+
 (use-package lsp-mode
   :commands lsp
-  :custom
-  (lsp-rust-analyzer-cargo-watch-command "clippy")
-  (lsp-eldoc-render-all t)
-  (lsp-idle-delay 0.6)
-  (lsp-rust-analyzer-server-display-inlay-hints t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
-  (lsp-rust-analyzer-display-chaining-hints t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
-  (lsp-rust-analyzer-display-closure-return-type-hints t)
-  (lsp-rust-analyzer-display-parameter-hints nil)
-  (lsp-rust-analyzer-display-reborrow-hints nil)
   :config
+  (setq lsp-signature-auto-activate t)
+  (setq lsp-signature-doc-lines 1) 
+  (setq lsp-signature-render-documentation nil)
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
 (use-package lsp-ui
