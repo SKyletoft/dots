474cd1ca4023bc9d7e5843be98c47ef2215494b7
Samuel Kyletoft <samuel@kyletoft.se>
2022-12-22 02:01:03 +0100

    Custom discord desktop file to force gpu accel

--- home.nix
+++ home.nix
@@ -387,6 +387,7 @@ in {
 		# Extra desktop files
 		".local/share/applications/signal-background.desktop".source = ./signal-background.desktop;
 		".local/share/applications/emacs.desktop".source             = ./emacsclient.desktop;
+		".local/share/applications/discord.desktop".source           = ./discord.desktop;
 
 		# Folders
 		".themes/yaru".source                         = ./.themes/yaru;
