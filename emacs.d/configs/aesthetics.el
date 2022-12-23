5f1c041ec5bc8ab0ea5f3beb7bc09128745fe036
Samuel Kyletoft <samuel@kyletoft.se>
2022-12-23 23:37:34 +0100

    Emacs: Drop the higher opacity on defocus thing

--- emacs.d/configs/aesthetics.el
+++ emacs.d/configs/aesthetics.el
@@ -3,9 +3,9 @@
 
 (set-frame-parameter (selected-frame)
                      'alpha
-                     '(90 . 95))
+                     '(90 . 90))
 (add-to-list 'default-frame-alist
-             '(alpha . (90 . 95)))
+             '(alpha . (90 . 90)))
 
 (load-theme 'custom-monokai t)
 (global-display-line-numbers-mode t)
