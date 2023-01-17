933b9b02d01bb6130b8e69a01db889d321946375
Samuel Kyletoft <samuel@kyletoft.se>
2023-01-17 21:43:30 +0100

    Move aesthetics load to function

--- emacs.d/configs/aesthetics.el
+++ emacs.d/configs/aesthetics.el
@@ -1,14 +1,22 @@
 (provide 'aesthetics)
-(add-to-list 'default-frame-alist '(font . "Fira Code Nerd Font-11"))
-(add-to-list 'default-frame-alist '(undecorated-round . t))
 
-(set-frame-parameter (selected-frame)
-                     'alpha
-                     '(90 . 90))
-(add-to-list 'default-frame-alist
-             '(alpha . (90 . 90)))
+(defun monokai-purple ()
+  (add-to-list 'default-frame-alist '(font . "Fira Code Nerd Font-11"))
+  (add-to-list 'default-frame-alist '(undecorated-round . t))
 
-(load-theme 'custom-monokai t)
+  (set-frame-parameter (selected-frame)
+                       'alpha
+                       '(90 . 90))
+  (add-to-list 'default-frame-alist
+               '(alpha . (90 . 90)))
+
+  (load-theme 'custom-monokai t)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+  (telephone-line-mode)
+  (setq-default truncate-lines t))
+
+(monokai-purple)
 
 ;; Disable background when in terminal
 (defun on-after-init ()
@@ -17,10 +25,17 @@
 (add-hook 'window-setup-hook 'on-after-init)
 (add-hook 'server-switch-hook 'on-after-init)
 
-;;(use-package tree-sitter
-(global-tree-sitter-mode)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-;; )
+;; Set default, fixed and variabel pitch fonts
+;; Use M-x menu-set-font to view available fonts
+(use-package mixed-pitch
+  :hook
+  (text-mode . mixed-pitch-mode)
+  :config
+  (set-face-attribute 'default nil :font "Fira Code Nerd Font" :height 110)
+  (set-face-attribute 'fixed-pitch nil :font "Fira Code Nerd Font")
+  (set-face-attribute 'variable-pitch nil :font "TeX Gyre Heros" :height 150))
+
+;; (add-hook 'mixed-pitch-mode-hook #'solaire-mode-reset)
 
 (use-package ligature
   :load-path "path-to-ligature-repo"
