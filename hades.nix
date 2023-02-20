95cc5823986c75f12200396e50e8987ed4ed9beb
Samuel Kyletoft <samuel@kyletoft.se>
2023-02-20 19:39:03 +0100

    Remove comments

--- hades.nix
+++ hades.nix
@@ -1,9 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 let
 	waylandSupport = false;
 	windowsFonts = false;
@@ -54,14 +49,12 @@ in {
 			dates = "weekly";
 			options = "--delete-older-than 30d";
 		};
-		# Free up to 10 GiB whenever there is less than 1GiB left.
 		extraOptions = ''
 			min-free = ${toString (1024 * 1024 * 1024)}
 			experimental-features = nix-command flakes
 		'';
 	};
 
-	# Use the systemd-boot EFI boot loader.
 	boot = {
 		loader = {
 			systemd-boot.enable = true;
@@ -93,12 +86,8 @@ in {
 		];
 	};
 
-	# Set your time zone.
 	time.timeZone = "Europe/Stockholm";
 
-	# The global useDHCP flag is deprecated, therefore explicitly set to false here.
-	# Per-interface useDHCP will be mandatory in the future, so this generated config
-	# replicates the default behaviour.
 	networking = {
 		hostName = "hades"; # Define your hostname.
 		# wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -108,13 +97,11 @@ in {
 			enp0s31f6.useDHCP = false; # WiFi card
 			# wlp0s20f0u1.useDHCP = false; # USB WiFi dongle
 		};
-		# Configure network proxy if necessary
 		# proxy = {
 		# 	default = "http://user:password@proxy:port/";
 		# 	noProxy = "127.0.0.1,localhost,internal.domain";
 		# };
 
-		# Open ports in the firewall.
 		firewall = {
 			allowedTCPPorts =
 				[ 80 443 6530 8000 8080 12825 ] # Development
@@ -122,17 +109,13 @@ in {
 			allowedUDPPorts =
 				[ 80 443 6530 8000 8080 12825 ] # Development
 				++ [ 53 1194 1195 1196 1197 1399 1391 1392 1393 1400 ]; # Mullvad
-			# Or disable the firewall altogether.
 			# enable = false;
 		};
 	};
 
-
-	# Select internationalisation properties.
 	# i18n.defaultLocale = "en_US.UTF-8";
 	console.useXkbConfig = true;
 
-	# VMM
 	virtualisation = {
 		libvirtd.enable = true;
 		spiceUSBRedirection.enable = true;
@@ -164,8 +147,6 @@ in {
 				Option         "AllowIndirectGLXProtocol" "off"
 				Option         "TripleBuffer" "on"
 			'';
-
-			# Enable touchpad support (enabled default in most desktopManager).
 			# libinput.enable = true;
 		};
 
@@ -211,10 +192,8 @@ in {
 
 		# flatpak.enable = true;
 
-		# Enable CUPS to print documents.
 		# printing.enable = true;
 
-		# Enable the OpenSSH daemon.
 		openssh = {
 			enable = true;
 			settings.PasswordAuthentication = false;
@@ -321,8 +300,6 @@ in {
 		corefonts
 	] else []);
 
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
 	programs = {
 		sway.enable = waylandSupport;
 		xwayland.enable = waylandSupport;
@@ -341,12 +318,6 @@ in {
 		adb.enable = true;
 	};
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 	system.stateVersion = "21.05"; # Did you read the comment?
 
 }
