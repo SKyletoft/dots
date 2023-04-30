a64b473f9076f65b8d09bc088c5cc9f6abda48a9
Samuel Kyletoft <samuel@kyletoft.se>
2023-04-30 05:48:34 +0200

    Medea: remove commented out cachix import

--- medea.nix
+++ medea.nix
@@ -12,7 +12,6 @@ in {
 	imports = [ # Include the results of the hardware scan.
 		/etc/nixos/hardware-configuration.nix
 		<nixos-hardware/lenovo/thinkpad/x1/yoga/7th-gen>
-		# /etc/nixos/cachix.nix
 	];
 
 	nixpkgs = {
