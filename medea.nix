28038a43ca9560f70cb895f49cfe71955acbd6b9
Samuel Kyletoft <samuel@kyletoft.se>
2023-04-30 05:49:31 +0200

    Medea: Run GC at 16 GiB

--- medea.nix
+++ medea.nix
@@ -54,9 +54,9 @@ in {
 			dates = "weekly";
 			options = "--delete-older-than 30d";
 		};
-		# Free up to 10 GiB whenever there is less than 1GiB left.
+		# Free up to 16 GiB whenever there is less than 1GiB left.
 		extraOptions = ''
-			min-free = ${toString (1024 * 1024 * 1024)}
+			min-free = ${toString (16 * 1024 * 1024 * 1024)}
 			experimental-features = nix-command flakes
 		'';
 	};
