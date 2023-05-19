4e32d751e9b7840215ca03b82b841209011bf61c
Samuel Kyletoft <samuel@kyletoft.se>
2023-05-19 04:39:57 +0200

    Emacs: ocaml hook: Add indent binding

--- emacs/configs/language-specifics.el
+++ emacs/configs/language-specifics.el
@@ -163,6 +163,7 @@
             (define-key evil-normal-state-map (kbd "SPC g") 'xref-find-definitions)
             (define-key evil-normal-state-map (kbd "SPC a") 'lsp-execute-code-action)
             (define-key evil-normal-state-map (kbd "SPC f") 'lsp-ui-doc-glance)
+            (define-key evil-normal-state-map (kbd "SPC i") 'indent-region)
             (editorconfig-apply)))
 
 (use-package pdf-tools
