a3c092bd142b5d8d89dcc84e93062eb025d0c6a0
Samuel Kyletoft <samuel@kyletoft.se>
2023-05-22 20:24:20 +0200

    Emacs Ocaml: Disable eldoc

--- emacs/configs/language-specifics.el
+++ emacs/configs/language-specifics.el
@@ -157,6 +157,7 @@
 (add-hook 'tuareg-mode-hook
           (lambda ()
             (set-indents 8 2 nil)
+            (setq-local eldoc-mode nil)
             (define-key evil-normal-state-map (kbd "SPC g") 'xref-find-definitions)
             (define-key evil-normal-state-map (kbd "SPC a") 'lsp-execute-code-action)
             (define-key evil-normal-state-map (kbd "SPC f") 'lsp-ui-doc-glance)
