e5c9f3b95caca6c24cadafbfe68387fafdfdab2f
Samuel Kyletoft <samuel@kyletoft.se>
2023-06-30 20:51:17 +0000

    Medea: Fix inputs style

--- medea/flake.nix
+++ medea/flake.nix
@@ -1,8 +1,6 @@
 {
 	inputs = {
-		nixpkgs = {
-			url = "github:NixOS/nixpkgs/nixos-unstable";
-		};
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		nixos-hardware.url = "github:NixOS/nixos-hardware";
 	};
 	outputs = { self, nixpkgs, nixos-hardware }: {
