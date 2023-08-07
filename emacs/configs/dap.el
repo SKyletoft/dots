72b848730f67ec69d5630f8e409e36d86093458f
Samuel Kyletoft <samuel@kyletoft.se>
2023-08-07 23:49:05 +0200

    Emacs: Remove haskell debug setup

--- emacs/configs/dap.el
+++ emacs/configs/dap.el
@@ -2,35 +2,7 @@
 
 (use-package dap-mode)
 
-(dap-register-debug-provider
-  "hda"
-  (lambda (conf)
-    (plist-put conf :dap-server-path (list
-                                      "haskell-debug-adapter"
-                                      "--hackage-version=0.0.31.0"))
-    conf))
-
 (require 'dap-gdb-lldb)
-
-(dap-register-debug-template "haskell-debug-adapter"
-  (list :type "hda"
-    :request "launch"
-    :name "haskell-debug-adapter"
-    :internalConsoleOptions "openOnSessionStart"
-    ;; :workspace (lsp-find-session-folder (lsp-session) (buffer-file-name))
-    ;; :workspace "C:/work/haskell/sample"
-    ;; :startup "C:/work/haskell/sample/app/Main.hs"
-    :startupFunc ""
-    :startupArgs ""
-    :stopOnEntry t
-    :mainArgs ""
-    ;; :ghciPrompt "H>>= "
-    ;; :ghciInitialPrompt "Prelude>"
-    ;; :ghciCmd "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show"
-    :ghciEnv (list :dummy "")
-    ;; :logFile "C:/work/haskell/sample/hdx4emacs.log"
-    :logLevel "WARNING"
-    :forceInspect nil))
 
 (dap-register-debug-template "Rust::GDB Run Configuration"
                              (list :type "gdb"
