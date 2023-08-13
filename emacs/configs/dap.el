6e71b14bff6c428ac915ad8e5bbeea46abf14cd8
Samuel Kyletoft <samuel@kyletoft.se>
2023-08-13 22:39:07 +0200

    Emacs: Debugging binds

--- emacs/configs/dap.el
+++ emacs/configs/dap.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (provide 'dap)
 
 (use-package dap-mode)
@@ -11,3 +12,36 @@
                                    :gdbpath "rust-gdb"
                                    :target nil
                                    :cwd nil))
+
+(define-minor-mode +dap-running-session-mode
+  "A mode for adding keybindings to running sessions"
+  nil
+  nil
+  (make-sparse-keymap)
+  (evil-normalize-keymaps) ;; if you use evil, this is necessary to update the keymaps
+  ;; The following code adds to the dap-terminated-hook
+  ;; so that this minor mode will be deactivated when the debugger finishes
+  (when +dap-running-session-mode
+    (let ((session-at-creation (dap--cur-active-session-or-die)))
+      (add-hook 'dap-terminated-hook
+                (lambda (session)
+                  (when (eq session session-at-creation)
+                    (+dap-running-session-mode -1)))))))
+
+;; Activate this minor mode when dap is initialized
+(add-hook 'dap-session-created-hook '+dap-running-session-mode)
+
+;; Activate this minor mode when hitting a breakpoint in another file
+(add-hook 'dap-stopped-hook '+dap-running-session-mode)
+
+;; Activate this minor mode when stepping into code in another file
+(add-hook 'dap-stack-frame-changed-hook (lambda (session)
+                                          (when (dap--session-running session)
+                                            (+dap-running-session-mode 1))))
+
+;; Debugging
+(evil-define-key 'normal '+dap-running-session-mode
+  (kbd "j") 'dap-step-out
+  (kbd "k") 'dap-step-next
+  (kbd "l") 'dap-step-in)
+(define-key evil-normal-state-map (kbd "C-b") 'dap-breakpoint-toggle)
