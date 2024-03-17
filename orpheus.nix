f342eb53be1e8717e8b29c20a90bff88d1449da4
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-17 07:52:53 +0100

    orpheus: Use a shell script instead of inline code

--- orpheus.nix
+++ orpheus.nix
@@ -143,23 +143,25 @@ in {
 		};
 	};
 
-	# Written by ChatGPT
-	systemd.services.autoUpdateConfig = {
-		wantedBy = [ "multi-user.target" ];
-		serviceConfig = {
-			Type =  "simple";
-			ExecStart = ''
-				${pkgs.runCommandNoCC "auto-update-config" { buildInputs = [ pkgs.git ]; } ""}
-				set -e
-				git fetch origin
-				if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]; then
-					git pull origin master
-					nixos-rebuild switch
-				fi
-			'';
-			WorkingDirectory = "/etc/nixos/dots";
+	systemd.services.autoUpdateConfig =
+		let the_script = pkgs.writeShellScript "update-system" ''
+			export PATH=${pkgs.lib.strings.makeBinPath [ pkgs.git ]}
+			set -e
+			cd /etc/nixos/dots
+			git reset --hard
+			git fetch origin
+			if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]; then
+				git pull origin master
+				nixos-rebuild switch
+			fi
+		'';
+		in {
+			wantedBy = [ "default.target" ];
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${the_script}";
+			};
 		};
-	};
 
 	programs = {
 		bash.shellInit = ''
