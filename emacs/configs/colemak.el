6ad9b3cecff37b3bff102b61a602a529247bb45e
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-24 10:44:57 +0100

    Emacs: Colemak: set-* → evil-define-key

--- emacs/configs/colemak.el
+++ emacs/configs/colemak.el
@@ -10,119 +10,139 @@
   (define-key evil-normal-state-map (kbd "C-M-i") 'evil-emacs-state)
   (define-key evil-normal-state-map (kbd "z x") 'execute-extended-command)
   (global-set-key (kbd "C-S-p") 'treemacs)
-  (set-nm (kbd "C-d") 'multi-vterm)
-  (set-nm (kbd "C-+") 'text-scale-increase)
-  (set-nm (kbd "C--") 'text-scale-decrease)
-  (define-key evil-normal-state-map (kbd "SPC m") 'magit)
-  (define-key evil-normal-state-map (kbd "SPC n") 'magit-blame)
-  (define-key evil-normal-state-map (kbd "SPC v") 'hs-toggle-hiding)
+  (evil-define-key '(normal motion) 'global
+    (kbd "C-d") 'multi-vterm
+    (kbd "C-+") 'text-scale-increase
+    (kbd "C--") 'text-scale-decrease)
   (define-key company-active-map (kbd "<tab>") 'my/snippet-complete-or-indent)
   (define-key company-active-map (kbd "<return>") 'newline)
-  (set-n (kbd "SPC å") 'projectile-command-map)
-  (set-n (kbd "ö") 'evil-ex)
-  (set-n (kbd "Ö") 'eval-expression)
-  (set-n (kbd "C-n") 'scratch-buffer)
+  (evil-define-key 'normal 'global
+    (kbd "SPC m") 'magit
+    (kbd "SPC n") 'magit-blame
+    (kbd "SPC v") 'hs-toggle-hiding
+    (kbd "SPC å") 'projectile-command-map
+    (kbd "ö") 'evil-ex
+    (kbd "Ö") 'eval-expression
+    (kbd "C-n") 'scratch-buffer)
 
-  ;; (define-key evil-normal-state-map (kbd "<escape>") 'evil-esc)
-  ;; (define-key evil-motion-state-map (kbd "<escape>") 'evil-esc)
-  ;; (define-key evil-visual-state-map (kbd "<escape>") 'evil-esc)
-  ;; (define-key evil-insert-state-map (kbd "<escape>") 'evil-esc)
-  ;; (define-key evil-symex-state-map  (kbd "<escape>") 'evil-esc)
+  (evil-define-key 'operator 'global
+    (kbd "u") evil-inner-text-objects-map
+    (kbd "y") evil-outer-text-objects-map)
+  (evil-define-key 'visual 'global
+    (kbd "u") evil-inner-text-objects-map
+    (kbd "y") evil-outer-text-objects-map)
 
-  (define-key evil-operator-state-map "u" evil-inner-text-objects-map)
-  (define-key evil-visual-state-map "u" evil-inner-text-objects-map)
-  (define-key evil-operator-state-map "y" evil-outer-text-objects-map)
-  (define-key evil-visual-state-map "y" evil-outer-text-objects-map)
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "r") 'evil-backward-char
+    (kbd "t") 'evil-forward-char
+    (kbd "f") 'evil-previous-visual-line
+    (kbd "s") 'evil-next-visual-line)
 
-  (set-nvm (kbd "r") 'evil-backward-char)
-  (set-nvm (kbd "t") 'evil-forward-char)
-  (set-nvm (kbd "f") 'evil-previous-visual-line)
-  (set-nvm (kbd "s") 'evil-next-visual-line)
+  (evil-define-key '(normal motion) 'global
+    (kbd "T") 'evil-forward-word-begin)
+  (evil-define-key 'visual 'global
+    (kbd "T") 'evil-forward-word-end)
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "F") (lambda () (interactive) (evil-scroll-line-up 5))
+    (kbd "S") (lambda () (interactive) (evil-scroll-line-down 5))
+    (kbd "R") 'evil-backward-word-begin)
 
-  (set-nvm (kbd "R") 'evil-backward-word-begin)
-  (set-nm  (kbd "T") 'evil-forward-word-begin)
-  (set-v   (kbd "T") 'evil-forward-word-end)
-  (set-nvm (kbd "F") (lambda () (interactive) (evil-scroll-line-up 5)))
-  (set-nvm (kbd "S") (lambda () (interactive) (evil-scroll-line-down 5)))
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "C-f") 'evil-jump-item
+    (kbd "C-s") 'evil-jump-paragraph)
+  (evil-define-key 'normal 'global
+    (kbd "p")   'evil-replace
+    (kbd "P")   'evil-enter-replace-state)
 
-  (set-nvm (kbd "C-f") 'evil-jump-item)
-  (set-nvm (kbd "C-s") 'evil-jump-paragraph)
-  (set-n   (kbd "p")   'evil-replace)
-  (set-n   (kbd "P")   'evil-enter-replace-state)
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "n")   'evil-insert
+    (kbd "l")   'evil-open-below
+    (kbd "L")   'evil-open-above
+    (kbd "e")   'evil-yank
+    (kbd "i")   'evil-delete)
+  (evil-define-key '(normal motion) 'global
+    (kbd "o")   'evil-paste-after
+    (kbd "M-v") 'evil-visual-block)
 
-  (set-nvm (kbd "n")   'evil-insert)
-  (set-nvm (kbd "l")   'evil-open-below)
-  (set-nvm (kbd "L")   'evil-open-above)
-  (set-nvm (kbd "e")   'evil-yank)
-  (set-nvm (kbd "i")   'evil-delete)
-  (set-nm  (kbd "o")   'evil-paste-after)
-  (set-nm  (kbd "M-v") 'evil-visual-block)
+  (evil-define-key '(normal motion insert) 'global
+    (kbd "<tab>")         'my/indent-line  ; For gui
+    [?\t]                 'my/indent-line) ; For terminal use
+  (evil-define-key '(normal motion insert emacs) 'global
+    (kbd "<backtab>")     'my/outdent-line
+    (kbd "<iso-lefttab>") 'my/outdent-line)
+  (evil-define-key 'visual 'global
+    (kbd "<tab>")         'my/indent-region
+    [?\t]                 'my/indent-region
+    (kbd "<backtab>")     'my/outdent-region
+    (kbd "<iso-lefttab")  'my/outdent-region)
 
-  (set-nmi  (kbd "<tab>")         'my/indent-line) ; For gui
-  (set-nmi  [?\t]                 'my/indent-line) ; For terminal use
-  (set-nmie (kbd "<backtab>")     'my/outdent-line)
-  (set-nmie (kbd "<iso-lefttab>") 'my/outdent-line)
-  (set-v    (kbd "<tab>")         'my/indent-region)
-  (set-v    [?\t]                 'my/indent-region)
-  (set-v    (kbd "<backtab>")     'my/outdent-region)
-  (set-v    (kbd "<iso-lefttab")  'my/outdent-region)
+  (evil-define-key '(normal visual) 'global
+    (kbd "A") 'align-regexp)
 
-  (set-nv (kbd "A") 'align-regexp)
-
-  (set-v (kbd "\"") 'wrap-in-quotes)
-  (set-v (kbd "'") 'wrap-in-apostrophes)
-  (set-v (kbd "`") 'wrap-in-ticks)
-  (set-v (kbd "(") 'wrap-in-parentheses)
-  (set-v (kbd "{") 'wrap-in-curlies)
-  (set-v (kbd "[") 'wrap-in-squares)
-  ;; (set-v (kbd "<") 'wrap-in-angles) ; Interferes with outdent
-  (set-v (kbd "$") 'wrap-in-dollar)
+  (evil-define-key 'visual 'global
+    (kbd "\"") 'wrap-in-quotes
+    (kbd "'") 'wrap-in-apostrophes
+    (kbd "`") 'wrap-in-ticks
+    (kbd "(") 'wrap-in-parentheses
+    (kbd "{") 'wrap-in-curlies
+    (kbd "[") 'wrap-in-squares
+    (kbd "<") 'wrap-in-angles
+    (kbd "$") 'wrap-in-dollar)
 
   (set-v (kbd "SPC u") 'comment-or-uncomment-region)
   (set-n (kbd "SPC u") 'comment-or-uncomment-line)
 
   ;; Find
-  (set-n  (kbd "SPC P") 'projectile-switch-project)
-  (set-n  (kbd "SPC p") 'projectile-find-file)
-  (set-n  (kbd "SPC d") 'ripgrep-regexp)
+  (evil-define-key 'normal 'global
+    (kbd "SPC P") 'projectile-switch-project
+    (kbd "SPC p") 'projectile-find-file
+    (kbd "SPC d") 'ripgrep-regexp)
 
   ;; Undo and redo
-  (set-nmi (kbd "C-q") 'evil-undo)
-  (set-nmi (kbd "C-S-q") 'evil-redo)
+  (evil-define-key '(normal motion insert) 'global
+    (kbd "C-q") 'evil-undo
+    (kbd "C-S-q") 'evil-redo)
 
-  (set-nm (kbd "C-r") 'evil-insert-line)
-  (set-nm (kbd "C-t") 'evil-append-line)
-  (set-nm (kbd "I") 'evil-delete-line)
-  (set-nm (kbd "u") nil)
-  (set-nm (kbd "&") nil)
-  ;; (define-key evil-symex-state-map (kbd "a") nil)
+  (evil-define-key '(normal motion) 'global
+    (kbd "C-r") 'evil-insert-line
+    (kbd "C-t") 'evil-append-line
+    (kbd "I") 'evil-delete-line
+    (kbd "u") nil
+    (kbd "&") nil)
 
-  (set-nme (kbd "C-p") 'split-window-horizontally)
-  (set-nme (kbd "C-w") 'split-window-vertically)
-  (set-nme (kbd "C-S-f") 'evil-window-up)
-  (set-nme (kbd "C-S-s") 'evil-window-down)
-  (set-nme (kbd "C-S-r") 'evil-window-left)
-  (set-nme (kbd "C-S-t") 'evil-window-right)
-  (set-nme (kbd "M-F") 'evil-window-decrease-height)
-  (set-nme (kbd "M-S") 'evil-window-increase-height)
-  (set-nme (kbd "M-T") 'evil-window-increase-width)
-  (set-nme (kbd "M-R") 'evil-window-decrease-width)
-  (set-n   (kbd "SPC g") 'transpose-with-treemacs)
-  (set-n   (kbd "SPC G") 'toggle-left-margin)
+  (evil-define-key '(normal motion emacs) 'global
+    (kbd "C-p") 'split-window-horizontally
+    (kbd "C-w") 'split-window-vertically
+    (kbd "C-S-f") 'evil-window-up
+    (kbd "C-S-s") 'evil-window-down
+    (kbd "C-S-r") 'evil-window-left
+    (kbd "C-S-t") 'evil-window-right
+    (kbd "M-F") 'evil-window-decrease-height
+    (kbd "M-S") 'evil-window-increase-height
+    (kbd "M-T") 'evil-window-increase-width
+    (kbd "M-R") 'evil-window-decrease-width)
+  (evil-define-key 'normal 'global
+    (kbd "SPC g") 'transpose-with-treemacs
+    (kbd "SPC G") 'toggle-left-margin)
 
-  (define-key evil-visual-state-map (kbd "SPC b") 'narrow-to-region)
-  (set-n (kbd "SPC b") 'widen)
+  (evil-define-key 'normal 'global
+    (kbd "SPC b") 'widen)
+  (evil-define-key 'visual 'global
+    (kbd "SPC b") 'narrow-to-region)
 
-  (set-nme (kbd "C-<tab>") 'next-buffer)
-  (set-nme [C-?\t] 'next-buffer)
-  (set-nme (kbd "C-<backtab>") 'previous-buffer)
-  (set-nme (kbd "C-<iso-lefttab>") 'previous-buffer)
-  (set-nv  (kbd "SPC <tab>") 'get-buffer-list)
+  (evil-define-key '(normal motion emacs) 'global
+    (kbd "C-<tab>") 'next-buffer
+    [C-?\t] 'next-buffer
+    (kbd "C-<backtab>") 'previous-buffer
+    (kbd "C-<iso-lefttab>") 'previous-buffer)
+  (evil-define-key '(normal visual) 'global
+    (kbd "SPC <tab>") 'get-buffer-list)
 
-  (set-nm (kbd "-") 'evil-search-forward)
-  (set-nm (kbd "_") 'evil-search-backward)
-  (set-nm (kbd "m") 'evil-search-next)
-  (set-nm (kbd "M") 'evil-search-prev)
+  (evil-define-key '(normal motion) 'global
+    (kbd "-") 'evil-search-forward
+    (kbd "_") 'evil-search-backward
+    (kbd "m") 'evil-search-next
+    (kbd "M") 'evil-search-prev)
 
   ;; Completion-buffer
   (define-key minibuffer-local-map (kbd "C-p") #'minibuffer-previous-completion)
@@ -130,10 +150,11 @@
   (define-key completion-in-region-mode-map (kbd "C-p") #'minibuffer-previous-completion)
   (define-key completion-in-region-mode-map (kbd "C-n") #'minibuffer-next-completion)
 
-  (set-n (kbd "SPC q") 'evil-quit)
-  (set-n (kbd "SPC Q") 'evil-save-modified-and-close)
-  (set-n (kbd "SPC w") 'save-buffer)
-  (set-n (kbd "SPC f") 'save-buffer)
+  (evil-define-key 'normal 'global
+    (kbd "SPC q") 'evil-quit
+    (kbd "SPC Q") 'evil-save-modified-and-close
+    (kbd "SPC w") 'save-buffer
+    (kbd "SPC f") 'save-buffer)
 
   (evil-define-key '(normal motion) evil-command-window-mode-map
     (kbd "C-g") 'evil-quit)
@@ -222,22 +243,25 @@
     (kbd "M-<f5>") 'rustic-cargo-test)
 
   (evil-define-key 'normal emacs-lisp-mode-map
+    (kbd "SPC o") 'indent-according-to-mode
     (kbd "SPC l") 'eval-buffer)
   (evil-define-key 'visual emacs-lisp-mode-map
+    (kbd "SPC o") 'indent-region
     (kbd "SPC l") 'eval-region)
 
   (evil-define-key '(normal visual) lisp-mode-shared-map
     (kbd "SPC i") 'describe-symbol
-    (kbd "SPC o") 'indent-according-to-mode
     (kbd "SPC t") 'paredit-forward-slurp-sexp
     (kbd "SPC T") 'paredit-forward-barf-sexp
     (kbd "SPC a") 'paredit-backward-slurp-sexp
     (kbd "SPC A") 'paredit-backward-barf-sexp)
 
   (evil-define-key 'normal scheme-mode-map
+    (kbd "SPC o") 'indent-according-to-mode
     (kbd "SPC l") 'slime-n
     (kbd "SPC L") 'slime-buf)
   (evil-define-key 'visual scheme-mode-map
+    (kbd "SPC o") 'indent-region
     (kbd "SPC l") 'slime-v)
 
   (evil-define-key 'visual sh-mode-map
