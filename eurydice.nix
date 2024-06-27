a8b64622a29b3e0255a8c32921fb764b5774734f
Samuel Kyletoft <samuel@kyletoft.se>
2024-06-28 01:03:34 +0200

    eurydice: Actually print hash to status

--- eurydice.nix
+++ eurydice.nix
@@ -230,7 +230,7 @@ in {
 					 + "&& SYSTEMD_COLORS=true systemctl status nginx | head -n3 >> /tmp/eurydice-status "
 					 + "&& SYSTEMD_COLORS=true systemctl status jellyfin | head -n3 >> /tmp/eurydice-status "
 					 + "&& SYSTEMD_COLORS=true systemctl status mullvad-daemon | head -n3 >> /tmp/eurydice-status "
-					 + "&& ${pkgs.git}/bin/git -C /etc/nixos/dots rev-parse HEAD"
+					 + "&& ${pkgs.git}/bin/git -C /etc/nixos/dots rev-parse HEAD >> /tmp/eurydice-status"
 					)
 					("*/05 * * * * enaya ${update-keys'} Enayaaa")
 					("*/05 * * * * u3836 ${update-keys'} SKyletoft")
