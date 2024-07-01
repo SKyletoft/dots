217f3fee782c03a901f82788296f1b9addc334de
Samuel Kyletoft <samuel@kyletoft.se>
2024-07-01 03:14:49 +0200

    Emacs: Aestethics: Use Nerdfonts

--- emacs/configs/aesthetics.el
+++ emacs/configs/aesthetics.el
@@ -20,13 +20,15 @@
   ;; (add-to-list 'default-frame-alist '(font . "Fira Code Nerd Font-11"))
   ;; (add-to-list 'default-frame-alist '(font . "0xProto-11"))
   ;; (add-to-list 'default-frame-alist '(font . "Monaspace Neon-11")) ;; åäö, wait are all comments in italics now?
-  (add-to-list 'default-frame-alist '(font . "Cascadia Code-11"))
   (add-to-list 'default-frame-alist '(undecorated-round . t))
   (setq-default line-spacing 0.1)
 
   (setq fill-column 80)
+  ;; (add-to-list 'default-frame-alist '(font . "Cascadia Code-22"))
+  (add-to-list 'default-frame-alist '(font . "Cascadia Code NF-11"))
   (when (not (or (string-match "PGTK" system-configuration-features)
                  (string-match "wayland" (getenv "XDG_SESSION_TYPE"))))
+    (add-to-list 'default-frame-alist '(font . "Cascadia Code NF-11"))
     (set-frame-parameter (selected-frame)
                          'alpha
                          '(90 . 90))
@@ -57,8 +59,8 @@
   :config
   (if (string-equal (system-name) "medea")
       ;; 2x gui scale on medea, but no built-in support for scaling this specific font
-      (set-face-attribute 'fringe nil :font "Cascadia Code" :height 220))
-  (set-face-attribute 'default nil :font "Cascadia Code" :height 110)
+      (set-face-attribute 'fringe nil :font "Cascadia Code NF" :height 220))
+  (set-face-attribute 'default nil :font "Cascadia Code NF" :height 110)
   (set-face-attribute 'fixed-pitch nil :font "Cascadia Code")
   (set-face-attribute 'variable-pitch nil :font "TeX Gyre Heros" :height 150))
 
