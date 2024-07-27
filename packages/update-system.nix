a9f555e74ae27e79742d9a73b9135c8e13e5bf83
Samuel Kyletoft <samuel@kyletoft.se>
2024-07-27 03:59:19 +0200

    update-system: nixos-rebuild doesn't support -L anymore?

--- packages/update-system.nix
+++ packages/update-system.nix
@@ -8,6 +8,6 @@ pkgs.writeShellScriptBin "update-system" ''
 	git fetch origin
 	if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]; then
 		git pull origin master
-		nixos-rebuild switch -L
+		nixos-rebuild switch
 	fi
 ''
