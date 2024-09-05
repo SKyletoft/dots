4896ac3b0c6a9fb792d3b81c63027004f01ce7f5
Samuel Kyletoft <samuel@kyletoft.se>
2024-09-05 15:58:18 +0200

    Emacs: Promela-2-mode: Give up on indents + add compile command

--- emacs/configs/promela-2-mode.el
+++ emacs/configs/promela-2-mode.el
@@ -1,5 +1,7 @@
-(defvar promela-indent-offset tab-width
-  "Indentation offset for `promela-2-mode'.")
+;;; promela-2-mode.el --- Syntax highlighting for Promela -*- lexical-binding: t -*-
+;;; Commentary:
+;;; It's as simple as possible.  I messed around a bit with indents and decided it was too much of a pain
+;;; Code:
 
 (defconst promela-2-highlights
   '(("/\\*\\(.\\|\n\\)*?\\*/\\|;" . 'font-lock-comment-face)
@@ -9,34 +11,9 @@
     ("\\b\\(active\\|if\\|fi\\|do\\|od\\|assert\\|goto\\|atomic\\|else\\|inline\\)\\b" . 'font-lock-keyword-face)
     ("#define\\|#include\\|#if\\|#endif\\|#else" . 'font-lock-preprocessor-face)))
 
-(defvar promela-smie-grammar
-  (smie-prec2->grammar
-   (smie-bnf->prec2
-    '((stmt ("if" exp "then" stmt "else" stmt)
-            ("while" exp "do" stmt)
-            ("{" stmts "}")
-            (stmt ";" stmt))
-      (stmts (stmt))
-      (exp ("true")
-           ("false")
-           ("!" exp)
-           (exp "&&" exp)
-           (exp "||" exp)
-           ("(" exp ")"))))))
-
-(defun promela-smie-rules (kind token)
-  (pcase (cons kind token)
-    (`(:elem . basic) promela-indent-offset)
-    (`(:before . ";") (smie-rule-separator kind))
-    (`(:after . "{") (smie-rule-parent))
-    (`(:before . "}") (smie-rule-parent))))
-
 (define-derived-mode promela-2-mode prog-mode "Promela"
   "major mode for editing promela code."
-  (setq font-lock-defaults '(promela-2-highlights))
-  (smie-setup promela-smie-grammar #'promela-smie-rules
-              :forward-token #'mylang-forward-token
-              :backward-token #'mylang-backward-token))
+  (setq font-lock-defaults '(promela-2-highlights)))
 
 (setq auto-mode-alist
       (append
@@ -44,4 +21,10 @@
              (cons "\\.spin$"     'promela-2-mode)
              (cons "\\.pml$"      'promela-2-mode))
        auto-mode-alist))
+
+(add-hook 'promela-2-mode-hook
+          (lambda ()
+            (setq-local compile-command (concat "spin " (buffer-file-name)))))
+
 (provide 'promela-2-mode)
+;;; promela-2-mode.el ends here.
