2c2cbb3c60416b26fbae523b1c4cdd7017a027a3
Samuel Kyletoft <samuel@kyletoft.se>
2024-09-22 20:21:06 +0200

    Update promela syntax

--- emacs/configs/promela-2-mode.el
+++ emacs/configs/promela-2-mode.el
@@ -4,11 +4,11 @@
 ;;; Code:
 
 (defconst promela-2-highlights
-  '(("/\\*\\(.\\|\n\\)*?\\*/\\|;" . 'font-lock-comment-face)
-    ("\\b\\(int\\|byte\\|bool\\|short\\|unsigned\\|chan\\)\\b" . 'font-lock-type-face)
+  '(("\\*\\(?:.\\|\n\\)*?\\*/\\|//\\(?:.\\|\n\\)*?$" . 'font-lock-comment-face)
+    ("\\b\\(proctype\\|int\\|byte\\|bool\\|short\\|unsigned\\|chan\\)\\b" . 'font-lock-type-face)
     ("[0-9]+" . 'font-lock-number-face)
     ("+\\|-\\|*\\|/\\|->\\|→\\|::\\|==\\|!\\|!=\\|<\\|>\\|<=\\|>==\\|!\\|?" . 'font-lock-operator-face)
-    ("\\b\\(active\\|if\\|fi\\|do\\|od\\|assert\\|goto\\|atomic\\|else\\|inline\\|of\\)\\b" . 'font-lock-keyword-face)
+    ("\\b\\(for\\|in\\|ltl\\|break\\|init\\|active\\|if\\|fi\\|do\\|od\\|assert\\|goto\\|atomic\\|else\\|inline\\|of\\)\\b" . 'font-lock-keyword-face)
     ("#define\\|#include\\|#if\\|#endif\\|#else" . 'font-lock-preprocessor-face)))
 
 (define-derived-mode promela-2-mode prog-mode "Promela"
