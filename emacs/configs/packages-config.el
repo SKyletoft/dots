9ccc7e3131803cf1537716bc6491c131f838f756
Samuel Kyletoft <samuel@kyletoft.se>
2024-09-22 05:26:21 +0200

    Emacs: Elsa-headers for packages-config.el

--- emacs/configs/packages-config.el
+++ emacs/configs/packages-config.el
@@ -1,6 +1,9 @@
-(provide 'packages-config)
+;;; packages-config.el --- Configuration of gnu elpa, melpa and use-package  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
 (require 'package)
+
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
@@ -10,3 +13,6 @@
 (eval-and-compile
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
+
+(provide 'packages-config)
+;;; packages-config.el ends here
