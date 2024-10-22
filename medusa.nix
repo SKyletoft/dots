2ead902fdbd127f19ec3e2c717bdfccc8274c43a
Samuel Kyletoft <samuel@kyletoft.se>
2024-10-22 17:43:10 +0200

    Medusa: Patch ollama to try to force gpu accel

--- medusa.nix
+++ medusa.nix
@@ -211,6 +211,10 @@ in {
 		ollama = {
 			enable = true;
 			acceleration = "rocm";
+			environmentVariables = {
+				HCC_AMDGPU_TARGET = "gfx1100";
+			};
+			rocmOverrideGfx = "11.0.0";
 		};
 	};
 
