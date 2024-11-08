13ecaa3821fe673c596db34928ae335d10fc40ae
Samuel Kyletoft <samuel@kyletoft.se>
2024-11-08 15:55:23 +0100

    Emacs: respect evil shift width in html-ts-mode

--- emacs/configs/language-specifics.el
+++ emacs/configs/language-specifics.el
@@ -28,6 +28,7 @@
 (defvaralias 'kotlin-mode-multiline-statement-offset      'evil-shift-width)
 (defvaralias 'sh-indentation                              'evil-shift-width)
 (defvaralias 'python-indent-offset                        'evil-shift-width)
+(defvaralias 'html-ts-mode-indent-offset                  'evil-shift-width)
 
 (defadvice align-regexp (around smart-tabs activate)
   (let ((indent-tabs-mode nil)) ad-do-it))
