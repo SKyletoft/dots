5e1de7ce9437dafd9afe523809e3873efab97971
Samuel Kyletoft <samuel@kyletoft.se>
2024-11-12 18:36:33 +0100

    lsp-booster: Fake aarch64 support

--- packages/lsp-booster.nix
+++ packages/lsp-booster.nix
@@ -1,16 +1,21 @@
 { stdenv
 , fetchurl
 , unzip
+, system
 }:
 
 stdenv.mkDerivation rec {
 	pname = "lsp-booster";
 	version = "v0.2.1";
 
-	src = fetchurl {
-		url = "https://github.com/blahgeek/emacs-lsp-booster/releases/download/${version}/emacs-lsp-booster_${version}_x86_64-unknown-linux-musl.zip";
-		sha256 = "f47745e6754eb5a54525da705ba2c83330ba5fbf6613e8ebbe728894fd0b468e";
-	};
+	src =
+		let arch = if system == "x86_64-linux"
+			then "x86_64-unknown-linux-musl"
+			else "aarch64-unknown-linux-musl";
+		in fetchurl {
+			url = "https://github.com/blahgeek/emacs-lsp-booster/releases/download/${version}/emacs-lsp-booster_${version}_${arch}.zip";
+			sha256 = "f47745e6754eb5a54525da705ba2c83330ba5fbf6613e8ebbe728894fd0b468e";
+		};
 
 	unpackPhase = ''
 		mkdir emacs-lsp-booster
