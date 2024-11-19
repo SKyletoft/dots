cdb8ea9088ad5ced538f8fc91122ec6b12e9667d
Samuel Kyletoft <samuel@kyletoft.se>
2024-11-19 01:14:58 +0100

    Emacs: Bad merge

--- emacs/configs/colemak.el
+++ emacs/configs/colemak.el
@@ -217,8 +217,6 @@
   (lang-with-lsp dafny-mode-map)
   (lang-with-lsp glsl-mode-map)
 
-  (define-key glsl-mode-map (kbd "S-<iso-lefttab>") 'ff-find-other-file 'remove)
-
   (evil-define-key '(normal visual) markdown-mode-map
     (kbd "SPC l")   'recompile
     (kbd "SPC L")   'compile)
