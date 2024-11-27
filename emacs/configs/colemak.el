c05b1df8a88f1da50a268320cdecb2e141378702
Samuel Kyletoft <samuel@kyletoft.se>
2024-11-27 06:47:46 +0100

    Emacs: Colemak: Snippet completions?

--- emacs/configs/colemak.el
+++ emacs/configs/colemak.el
@@ -71,8 +71,8 @@
     (kbd "o") 'evil-visual-paste)
 
   (evil-define-key '(normal motion insert) 'global
-    (kbd "<tab>") 'my/indent-line  ; For gui
-    [?\t]         'my/indent-line) ; For terminal use
+    (kbd "<tab>") 'my/snippet-complete-or-indent  ; For gui
+    [?\t]         'my/snippet-complete-or-indent) ; For terminal use
   (evil-define-key '(normal motion insert emacs) 'global
     (kbd "<backtab>")     'my/outdent-line
     (kbd "<iso-lefttab>") 'my/outdent-line)
