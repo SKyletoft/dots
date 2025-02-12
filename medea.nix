857431db5c266daa1e6d8d6045074cafb65d2085
Samuel Kyletoft <samuel@kyletoft.se>
2025-02-12 13:16:17 +0100

    medea: Add Haskell binary cache

--- medea.nix
+++ medea.nix
@@ -45,10 +45,12 @@ in {
 				"https://nix-community.cachix.org"
 				"https://cache.nixos.org/"
 				"https://nix.u3836.se/"
+				"https://cache.iog.io"
 			];
 			trusted-public-keys = [
 				"nix.u3836.se:t7H/bFWi14aBFYPE5A00eEQawd7Ssl/fXbq/2C+Bsrs="
 				"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+				"hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
 			];
 		};
 		gc = {
