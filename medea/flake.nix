3b076a9a93eee11664a7aa1abe70dc88390d2e98
Samuel Kyletoft <samuel@kyletoft.se>
2025-03-09 02:26:49 +0100

    medea: Flake: inherit inputs v4

--- medea/flake.nix
+++ medea/flake.nix
@@ -4,10 +4,10 @@
 		nixos-hardware.url = "github:NixOS/nixos-hardware";
 		nixGL.url = "github:nix-community/nixGL";
 	};
-	outputs = { self, nixpkgs, nixos-hardware, nixGL }@inputs: {
+	outputs = { self, nixpkgs, nixos-hardware, nixGL }@args: {
 		nixosConfigurations.medea = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
-			specialArgs = inputs;
+			specialArgs = args;
 			modules = [
 				../medea.nix
 				nixos-hardware.nixosModules.lenovo-thinkpad-x1-yoga-7th-gen
