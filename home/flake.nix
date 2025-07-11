95f06664654668b5c965807391fafb467c04c2cc
Samuel Kyletoft <samuel@kyletoft.se>
2025-07-12 00:09:43 +0200

    Home: Update Emacs

--- home/flake.nix
+++ home/flake.nix
@@ -8,7 +8,7 @@
 		vimPin.url = "github:NixOS/nixpkgs?rev=9813adc7f7c0edd738c6bdd8431439688bb0cb3d";
 		emacsPkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 		emacsOverlay = {
-			url = "github:nix-community/emacs-overlay?rev=5c793af459e1f5d4a36d41f32a4841a3d76b7e9e";
+			url = "github:nix-community/emacs-overlay?rev=fcd9c1b4bded92ec89abe7b41cb2fdb1dd1dd370";
 			inputs.nixpkgs.follows = "emacsPkgs";
 			inputs.nixpkgs-stable.follows = "emacsPkgs";
 		};
