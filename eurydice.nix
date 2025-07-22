395e1466c4babb1083d55d0fa6e56b05cf0fff43
Samuel Kyletoft <samuel@kyletoft.se>
2025-07-22 02:22:03 +0200

    Eurydice: Set status page back to dark mode

--- eurydice.nix
+++ eurydice.nix
@@ -166,7 +166,7 @@ in {
 
 					("* * * * * u3836   ${update-motd}/bin/update-motd")
 					("*/05 * * * * root ${update-website}/bin/update-website")
-					("* * * * * root    cat /tmp/eurydice-status | ${pkgs.ansi2html}/bin/ansi2html -la -t \"Eurydice Status\" > /var/www/status/index.html")
+					("* * * * * root    cat /tmp/eurydice-status | ${pkgs.ansi2html}/bin/ansi2html -t \"Eurydice Status\" > /var/www/status/index.html")
 				];
 		};
 		nix-serve = {
