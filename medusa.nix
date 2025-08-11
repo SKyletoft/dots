39f602af823ac7d02142990154223d276cf01630
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-12 01:31:36 +0200

    Medusa: Add context length to ollama

--- medusa.nix
+++ medusa.nix
@@ -83,6 +83,7 @@
 				HCC_AMDGPU_TARGET = "gfx1100";
 				OLLAMA_GPU_MEMORY_FRACTION = "0.8"; # Only allow 80% VRAM usage
 				OLLAMA_GPU_OVERHEAD = "2147483648"; # Leave 2GB for non-llm tasks, yes this conflicts with above
+				OLLAMA_CONTEXT_LENGTH = "65536";    # Extend context length to 64k
 			};
 			rocmOverrideGfx = "11.0.0";
 		};
