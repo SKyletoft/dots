bb1960b8d50b1d54b70f2cb59b78a8c70b7090da
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-11 19:44:12 +0200

    update-system: Fix if
    (it used to always fire)

--- packages/update-system.nix
+++ packages/update-system.nix
@@ -4,10 +4,9 @@ pkgs.writeShellScriptBin "update-system" ''
 	export PATH=${pkgs.lib.strings.makeBinPath (with pkgs; [ git nixos-rebuild systemd ]) }
 	set -e
 	cd /etc/nixos/dots
-	git reset --hard HEAD~10
 	git fetch origin
 	if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]; then
-		git pull origin master
+		git reset --hard origin/master
 		nixos-rebuild switch -L
 	fi
 ''
