4a2fa8c2ef1e6bc42ffc137e25a66e5188127835
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-13 16:51:00 +0200

    Persephone: Copy in minimal parts of common-system

--- persephone.nix
+++ persephone.nix
@@ -1,7 +1,12 @@
 { config, pkgs, lib, nixGL, waylandSupport, windowsFonts, nativeBuild, ... }:
 
 {
+	nix.extraOptions = ''
+		experimental-features = nix-command flakes
+	'';
+
 	networking.hostName = "persephone";
+	time.timeZone = "Europe/Stockholm";
 
 	powerManagement.powertop.enable = true;
 
@@ -20,6 +25,14 @@
 	#	];
 	#	shell = pkgs.bash;
 	# };
+
+	environment.systemPackages = with pkgs; [
+		neovim
+		git
+		setup-system
+		update-keys
+		update-system
+	];
 
 	system.stateVersion = "21.11";
 }
