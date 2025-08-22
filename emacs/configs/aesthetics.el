a292c0863a0c968f7657601aeb9d43a81b1d4c3c
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-23 00:33:42 +0200

    Emacs: Hopefully fix magit decreasing text size

--- emacs/configs/aesthetics.el
+++ emacs/configs/aesthetics.el
@@ -75,8 +75,8 @@
   :config
   (if (string-equal (system-name) "medea")
       ;; 2x gui scale on medea, but no built-in support for scaling this specific font
-      (set-face-attribute 'fringe nil :font "Cascadia Code NF" :height 220))
-  (set-face-attribute 'default nil :font "Cascadia Code NF" :height 110)
+      (set-face-attribute 'fringe nil :font "Cascadia Code NF" :height 240))
+  (set-face-attribute 'default nil :font "Cascadia Code NF" :height 120)
   (set-face-attribute 'fixed-pitch nil :font "Cascadia Code")
   (set-face-attribute 'variable-pitch nil :font "TeX Gyre Heros" :height 150))
 
