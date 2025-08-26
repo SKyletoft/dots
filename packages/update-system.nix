a60f107f823522e8e52d075989adb1b0d5e48c22
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-26 20:56:10 +0200

    update-system/hms: Use nom

--- packages/update-system.nix
+++ packages/update-system.nix
@@ -1,12 +1,12 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "update-system" ''
-	export PATH=${pkgs.lib.strings.makeBinPath (with pkgs; [ git nixos-rebuild systemd ]) }
+	export PATH=${pkgs.lib.strings.makeBinPath (with pkgs; [ git nixos-rebuild nix-output-monitor systemd ]) }
 	set -e
 	cd /etc/nixos/dots
 	git fetch origin
 	if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]; then
 		git reset --hard origin/master
-		nixos-rebuild switch -L
+		nixos-rebuild switch -L --json |& nom
 	fi
 ''
