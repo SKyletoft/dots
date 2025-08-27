c74e0fa48b11912900bde44a0f49f0a9a4bbf33e
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-27 02:15:03 +0200

    overlays: Remove 1441 mutter patch (triple buffering)

--- overlays.nix
+++ overlays.nix
@@ -4,11 +4,6 @@ nativeBuild: nativeArch:
 		cascadia-code-greek = super.cascadia-code.overrideAttrs(old: {
 			url = "";
 		});
-		# gnome = super.gnome.overrideScope' (gself: gsuper: {
-			# mutter = gsuper.mutter.overrideAttrs (oldAttrs: {
-				# patches = [ ./1441.patch ] ++ (oldAttrs.patches or []);
-			# });
-		# });
 		wlroots = super.wlroots.overrideAttrs(old: {
 			postPatch = "sed -i 's/assert(argb8888 &&/assert(true || argb8888 ||/g' 'render/wlr_renderer.c'";
 		});
