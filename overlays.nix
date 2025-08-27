1e83ebd10e309db9d378367fdb703206fa983320
Samuel Kyletoft <samuel@kyletoft.se>
2025-08-27 02:16:32 +0200

    overlays: Remove unused font and lorri overlays

--- overlays.nix
+++ overlays.nix
@@ -1,21 +1,5 @@
 nativeBuild: nativeArch:
-[
-	(self: super: {
-		cascadia-code-greek = super.cascadia-code.overrideAttrs(old: {
-			url = "";
-		});
-	})
-	(final: prev: {
-		lorri = prev.lorri.overrideAttrs(old: {
-			src = prev.fetchFromGitHub {
-				owner = "kini";
-				repo = old.pname;
-				rev = "6fe667e5d63e6e347b545c9f28c9da15a7c383e0";
-				sha256 = "sha256-bBxXhN09fhUv8BT/bm2n2CGjwA+AUnbXSSW0UE8dqTg=";
-			};
-		});
-	})
-] ++ (if nativeBuild then [
+[] ++ (if nativeBuild then [
 	(final: prev: {
 		linuxPackages_xanmod = prev.linuxPackages_xanmod.extend (kfinal: kprev: {
 			kernel = (kprev.kernel.override {
