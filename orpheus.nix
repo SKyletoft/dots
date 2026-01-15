bfd0ad9aa1896a07748e3e945a58df3744f009c0
Samuel Kyletoft <samuel@kyletoft.se>
2026-01-15 02:29:13 +0100

    Orpheus: Use transmission 4 explicitly

--- orpheus.nix
+++ orpheus.nix
@@ -19,6 +19,7 @@
 		transmission = {
 			enable = true;
 			openRPCPort = true;
+			package = pkgs.transmission_4;
 			settings = {
 				rpc-bind-address = "0.0.0.0";
 				rpc-whitelist = "127.0.0.1,192.168.*.*";
