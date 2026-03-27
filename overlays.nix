92f3869b532e4db1fd4e07258b6a1a427a3fcfe6
Samuel Kyletoft <samuel@kyletoft.se>
2026-03-27 16:25:14 +0100

    system: Disable -march native kernel + update system

--- overlays.nix
+++ overlays.nix
@@ -1,15 +1,15 @@
 nativeBuild: nativeArch:
 [] ++ (if nativeBuild then [
-	(final: prev: {
-		linuxPackages_xanmod = prev.linuxPackages_xanmod.extend (kfinal: kprev: {
-			kernel = (kprev.kernel.override {
-				name = "linux-xanmod-native";
-				extraMakeFlags = [
-					"KCFLAGS+=-O3"
-					("KCFLAGS+=-march=" + nativeArch)
-					("KCFLAGS+=-mtune=" + nativeArch)
-				];
-			});
-		});
-	})
+	# (final: prev: {
+	#	linuxPackages_xanmod = prev.linuxPackages_xanmod.extend (kfinal: kprev: {
+	#		kernel = (kprev.kernel.override {
+	#			name = "linux-xanmod-native";
+	#			extraMakeFlags = [
+	#				"KCFLAGS+=-O3"
+	#				("KCFLAGS+=-march=" + nativeArch)
+	#				("KCFLAGS+=-mtune=" + nativeArch)
+	#			];
+	#		});
+	#	});
+	# })
 ] else [])
